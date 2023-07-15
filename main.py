96f1b8ebb94e50bd48084bd6d97a6abd74b1853a
Sander124 <85620656+Sander124@users.noreply.github.com>
2023-07-15 17:00:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ if "cvalues" not in st.session_state:
 def increment_counter(i):
     st.session_state.cvalues[i] += 1
 
+def enable_counter(i):
+    col[i][0].disabled = False
+    col[i][1].disabled = False
     
 with col[0]:
     st.button(f"Increment {counters[0]}", on_click=increment_counter, args=[0]) 
@@ -27,11 +30,15 @@ with col[3]:
     st.metric(counters[3], st.session_state.cvalues[3])
 
 with st.sidebar:
-    if st.checkbox(f"Enable {counters[1]}"):
+    counter2 = st.checkbox(f"Enable {counters[1]}")
+    counter3 = st.checkbox(f"Enable {counters[2]}")
+    counter4 = st.checkbox(f"Enable {counters[3]}")
+    
+    if counter2:
         enable_counter(1)
-    if st.checkbox(f"Enable {counters[2]}"):
+    if counter3:
         enable_counter(2)
-    if st.checkbox(f"Enable {counters[3]}"):
+    if counter4:
         enable_counter(3)
             
         custom_name = st.text_input(f"New name for {counters[i]}")
