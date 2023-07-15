787e447bec457bc7caa2af3ee450ee9858efac96
Sander124 <85620656+Sander124@users.noreply.github.com>
2023-07-15 17:46:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ if 'cnames' not in st.session_state:
 
 def increment_counter(i):
     st.session_state.cvalues[i] += 1
+
+def reset_counters():
+    st.session_state.cvalues = [0, 0, 0, 0]
+    
     
 with col[0]:
     st.button(f"Increment {st.session_state.cnames[0]}", on_click=increment_counter, args=[0], key = 1) 
@@ -36,6 +40,11 @@ with st.sidebar:
     #custom_name3 = st.text_input(f"New name for {st.session_state.cnames[2]}")
     #custom_name4 = st.text_input(f"New name for {st.session_state.cnames[3]}")
 
+    reset_knop = st.button('Reset counters')
+    if reset_knop:
+        reset_counters()
+
+    
     with st.form('form1'):
         to_name = st.text_input(f"Replace first name with:")
         apply = st.form_submit_button("Apply changes")
